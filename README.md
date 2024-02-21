d01ba53dcf83e56e19140e62843353a4a9b75801
Seung taek Oh <elqkddl@naver.com>
2024-02-21 15:36:43 +0900

    [Feat] Ref를 통해 코드를 줄이기 전(input과 button)

---
+++ README.md
@@ -0,0 +1,16 @@
+# CountDown-Game
+
+> React 기초 다지기 공부용 프로젝트
+> Demo : 페이지
+
+## 실행
+
+```shell
+npm insall
+npm run dev
+```
+
+## 사용
+
+- React
+- Vite
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import {useState} from 'react';
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState('');
+  const [submit, setSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    setSubmit(false); // 입력창에 타이핑을 치자마자 false값으로 두기 위해서 사용
+    setPlayerName(e.target.value);
+    console.log(playerName);
+  };
+
+  const handleClick = () => {
+    setSubmit(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submit ? playerName : 'unknown'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
