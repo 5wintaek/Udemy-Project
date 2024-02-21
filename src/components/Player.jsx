47900df4732ab8c0a2ea765a0c96a4a94899c35e
Seung taek Oh <elqkddl@naver.com>
2024-02-21 16:09:16 +0900

    [Feat] Ref를 생성하여 간소화 시키기

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,19 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 
 export default function Player() {
+  const input = useRef();
   const [playerName, setPlayerName] = useState('');
-  const [submit, setSubmit] = useState(false);
-
-  const handleChange = (e) => {
-    setSubmit(false); // 입력창에 타이핑을 치자마자 false값으로 두기 위해서 사용
-    setPlayerName(e.target.value);
-    console.log(playerName);
-  };
 
   const handleClick = () => {
-    setSubmit(true);
+    // setSubmit(true);
+    setPlayerName(input.current.value);
   };
 
   return (
     <section id="player">
-      <h2>Welcome {submit ? playerName : 'unknown'}</h2>
+      <h2>Welcome {playerName ? playerName : 'unknown'}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={playerName} />
+        <input ref={input} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
