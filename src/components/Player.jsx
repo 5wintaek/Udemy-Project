dd470ad5d18476731ba25af0609da64a7301a89a
Seung taek Oh <elqkddl@naver.com>
2024-03-06 19:25:46 +0900

    [Feat] 상태와 참조를 이용해서 작업하기(1)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,6 +7,7 @@ export default function Player() {
   const handleClick = () => {
     // setSubmit(true);
     setPlayerName(input.current.value);
+    input.current.value = '';
   };
 
   return (
@@ -19,3 +20,7 @@ export default function Player() {
     </section>
   );
 }
+
+// state 와 Ref 의 차이점 알아보기
+// 상태 : 상태값들은 컴포넌트 함수를 재실행함 상태는 UI에 바로 반영되어야 하는 값들이 있을때만 사용
+// 참조 :시스템 내부에 보이지 않는 쪽, UI에 직접적인 영향을 끼치지 않는 것들에 사용
