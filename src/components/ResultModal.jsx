7bc1f7ad34f898ca79d784c7f8d1db5f3d679199
Seung taek Oh <elqkddl@naver.com>
2024-03-13 09:40:42 +0900

    [Feat] Modal 컴포넌트 생성 : 참조의 도움을 받아 modal창 흐리게 해보기(후)

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,9 @@
-function ResultModal({result, targetTime}) {
+import {forwardRef} from 'react';
+
+const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
   return (
     // dialog 는 기본값이 close라 open 이라는 것을 넣어줘야함
-    <dialog className="result-modal" open>
+    <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
       <p>The target time was {targetTime} seconds.</p>
       <p>You Stop the timer with X seconds lefts</p>
@@ -10,6 +12,6 @@ function ResultModal({result, targetTime}) {
       </form>
     </dialog>
   );
-}
+});
 
 export default ResultModal;
