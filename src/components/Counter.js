48475042dfeba9b324891e0d37c5569344651a43
Seung taek Oh <elqkddl@naver.com>
2024-05-17 10:54:49 +0900

    [Redux] 291.리덕스 데이터 사용하기

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,19 @@
 import { useSelector } from "react-redux";
 // useStore 과 useSelector 차이점 알아보기
+// useStore 는 저장소에 직접 액세스 할 수 있음
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
+  // 리덕스가 관리하는 상태를 받는것.
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import { Provider } from "react-redux"; // store를 제공하기 위해 사용
 
 import "./index.css";
 import App from "./App";
